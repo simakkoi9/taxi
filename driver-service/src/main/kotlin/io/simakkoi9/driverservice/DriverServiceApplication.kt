04d517910b56a44868f8549dee903df8871c054c
AniKi9 <romashimanchik04@gmail.com>
2025-01-21 21:31:45 +0300

    taxi git init

---
+++ driver-service/src/main/kotlin/io/simakkoi9/driverservice/DriverServiceApplication.kt
@@ -0,0 +1,11 @@
+package io.simakkoi9.driverservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DriverServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<DriverServiceApplication>(*args)
+}
---
+++ driver-service/src/test/kotlin/io/simakkoi9/driverservice/DriverServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.simakkoi9.driverservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DriverServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
