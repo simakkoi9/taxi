6b8cca1a773a74e31ff0b0142b148b099e76188b
AniKi9 <romashimanchik04@gmail.com>
2025-01-23 17:33:24 +0300

    migrations

---
+++ passenger-service/src/main/resources/db/changelog/v1/passengers.sql
@@ -0,0 +1,17 @@
+
+drop type if exists user_status;
+create type user_status as enum ('ACTIVE', 'DELETED');
+
+drop table if exists passengers;
+create table passengers
+(
+    id bigserial
+        constraint passengers_pk primary key                    not null,
+    name       varchar(255)                                     not null,
+    email      varchar(255)                                     not null,
+    phone      varchar(255)                                     not null,
+    status     user_status    default 'ACTIVE'::user_status     not null,
+    created_at timestamp default now()                          not null
+);
+
+
