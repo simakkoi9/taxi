866ba706ac5c60c922b33c2159dbfb6aa45521c3
AniKi9 <romashimanchik04@gmail.com>
2025-01-24 19:20:13 +0300

    sql default values deleted

--- passenger-service/src/main/resources/db/changelog/v1/passengers.sql
+++ passenger-service/src/main/resources/db/changelog/v1/passengers.sql
@@ -1,16 +1,13 @@
 
-drop type if exists user_status;
-create type user_status as enum ('ACTIVE', 'DELETED');
-
 create table if not exists passengers
 (
     id bigserial
-        constraint passengers_pk primary key                    not null,
-    name       varchar(255)                                     not null,
-    email      varchar(255)                                     not null,
-    phone      varchar(255)                                     not null,
-    status     user_status    default 'ACTIVE'::user_status     not null,
-    created_at timestamp default now()                          not null
+        constraint passengers_pk primary key    not null,
+    name       varchar                          not null,
+    email      varchar                          not null,
+    phone      varchar                          not null,
+    status     varchar                          not null,
+    created_at timestamp                        not null
 );
 
 
