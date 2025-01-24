c0294b9c042a0a53043a9e0a32100fa95afd774b
AniKi9 <romashimanchik04@gmail.com>
2025-01-24 16:50:24 +0300

    passenger repository

--- passenger-service/src/main/java/io/simakkoi9/passengerservice/repository/PassengerRepository.java
+++ passenger-service/src/main/java/io/simakkoi9/passengerservice/repository/PassengerRepository.java
@@ -1,9 +1,7 @@
 package io.simakkoi9.passengerservice.repository;
 
 import io.simakkoi9.passengerservice.model.entity.Passenger;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,5 +10,5 @@ import java.util.Optional;
 public interface PassengerRepository extends CrudRepository<Passenger, Long> {
 
     boolean existsByEmail(String email);
-    Optional<Passenger> findPassengerByEmail(String email);
+    Optional<Passenger> findByEmail(String email);
 }
