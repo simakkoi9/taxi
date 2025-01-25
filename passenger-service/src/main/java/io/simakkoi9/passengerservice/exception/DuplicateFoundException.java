13d70a880384e05964814bb0d62f6185551a625f
AniKi9 <romashimanchik04@gmail.com>
2025-01-25 16:53:15 +0300

    exceptions

--- passenger-service/src/main/java/io/simakkoi9/passengerservice/exception/DuplicateFoundException.java
+++ passenger-service/src/main/java/io/simakkoi9/passengerservice/exception/DuplicateFoundException.java
@@ -1,5 +1,7 @@
 package io.simakkoi9.passengerservice.exception;
 
 public class DuplicateFoundException extends RuntimeException{
-
+    public DuplicateFoundException(String message){
+        super(message);
+    }
 }
--- passenger-service/src/main/java/io/simakkoi9/passengerservice/exception/ResourceNotFoundException.java
+++ passenger-service/src/main/java/io/simakkoi9/passengerservice/exception/ResourceNotFoundException.java
@@ -1,4 +1,7 @@
 package io.simakkoi9.passengerservice.exception;
 
 public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
 }
