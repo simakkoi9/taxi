2fbc7abf4cf7ec3cff9f9f84467563f3aadb8815
AniKi9 <romashimanchik04@gmail.com>
2025-01-27 01:15:16 +0300

    repositories and services

---
+++ driver-service/src/main/kotlin/io/simakkoi9/driverservice/repository/CarRepository.kt
@@ -0,0 +1,14 @@
+package io.simakkoi9.driverservice.repository;
+
+import io.simakkoi9.driverservice.model.entity.Car
+import io.simakkoi9.driverservice.model.entity.UserStatus
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+import java.util.Optional
+
+@Repository
+interface CarRepository : CrudRepository<Car, Long> {
+    fun findByIdAndStatus(id: Long, status: UserStatus): Optional<Car>
+
+    fun findAllByStatus(status: UserStatus): Iterable<Car>
+}
---
+++ driver-service/src/main/kotlin/io/simakkoi9/driverservice/repository/DriverRepository.kt
@@ -0,0 +1,15 @@
+package io.simakkoi9.driverservice.repository;
+
+import io.simakkoi9.driverservice.model.entity.Driver
+import io.simakkoi9.driverservice.model.entity.UserStatus
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+import java.util.Optional
+
+@Repository
+interface DriverRepository : CrudRepository<Driver, Long> {
+
+    fun findByIdAndStatus(id: Long, status: UserStatus): Optional<Driver>
+
+    fun findAllByStatus(status: UserStatus): Iterable<Driver>
+}
