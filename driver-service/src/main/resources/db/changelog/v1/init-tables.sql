3df42bcea999a22f1e08efd2db00f2dc05e08513
AniKi9 <romashimanchik04@gmail.com>
2025-01-28 18:42:09 +0300

    createdAt added

--- driver-service/src/main/resources/db/changelog/v1/init-tables.sql
+++ driver-service/src/main/resources/db/changelog/v1/init-tables.sql
@@ -5,18 +5,20 @@ CREATE TABLE IF NOT EXISTS cars
     model      VARCHAR                          NOT NULL,
     color      VARCHAR                          NOT NULL,
     number     VARCHAR                          NOT NULL,
-    status     INTEGER                          NOT NULL
+    status     INTEGER                          NOT NULL,
+    created_at TIMESTAMP                        NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS drivers
 (
-    id     BIGSERIAL CONSTRAINT drivers_pk      PRIMARY KEY,
-    name   VARCHAR                              NOT NULL,
-    email  VARCHAR                              NOT NULL,
-    phone  VARCHAR                              NOT NULL,
-    gender INTEGER                              NOT NULL,
-    car_id BIGINT CONSTRAINT drivers_cars_id_fk REFERENCES cars,
-    status INTEGER                              NOT NULL
+    id          BIGSERIAL CONSTRAINT drivers_pk      PRIMARY KEY,
+    name        VARCHAR                              NOT NULL,
+    email       VARCHAR                              NOT NULL,
+    phone       VARCHAR                              NOT NULL,
+    gender      INTEGER                              NOT NULL,
+    car_id      BIGINT CONSTRAINT drivers_cars_id_fk REFERENCES cars,
+    status      INTEGER                              NOT NULL,
+    created_at  TIMESTAMP                            NOT NULL
 );
 
 
