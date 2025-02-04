9db01837a39f03328f710e48c250f1f961207059
AniKi9 <romashimanchik04@gmail.com>
2025-02-04 15:44:31 +0300

    domain model

--- rating-service/src/main/java/io/simakkoi9/ratingservice/model/entity/Rating.java
+++ rating-service/src/main/java/io/simakkoi9/ratingservice/model/entity/Rating.java
@@ -1,7 +1,11 @@
 package io.simakkoi9.ratingservice.model.entity;
 
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,14 +20,25 @@ import lombok.ToString;
 @ToString
 @Entity
 @Table(name = "rating")
-public class Rating extends PanacheEntity {
+public class Rating extends PanacheEntityBase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "ride_id")
     private String rideId;
 
+    @Column(name = "rate_for_driver")
     private Integer rateForDriver;
 
+    @Column(name = "rate_for_passenger")
     private Integer rateForPassenger;
 
+    @Column(name = "comment_for_driver")
     private String commentForDriver;
 
+    @Column(name = "comment_for_passenger")
     private String commentForPassenger;
+
 }
--- rating-service/src/main/resources/db/migration/V1__init_table.sql
+++ rating-service/src/main/resources/db/migration/V1__init_table.sql
@@ -1,8 +1,8 @@
+
 CREATE TABLE rating
 (
-    id                    BIGSERIAL             NOT NULL,
-        CONSTRAINT pk_rating PRIMARY KEY (id),
-    ride_id               VARCHAR(24)           NOT NULL,
+    id                    BIGSERIAL PRIMARY KEY     NOT NULL,
+    ride_id               VARCHAR(24)               NOT NULL,
     rate_for_driver       INTEGER,
     rate_for_passenger    INTEGER,
     comment_for_driver    VARCHAR(240),
