631b427dcc767e21e5c444d862c97d9d6ebe1e9e
AniKi9 <romashimanchik04@gmail.com>
2025-04-14 18:11:12 +0300

    auth-service

---
+++ auth-service/src/main/java/io/simakkoi9/authservice/controller/SecurityController.java
@@ -0,0 +1,10 @@
+package io.simakkoi9.authservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class SecurityController {
+
+}
